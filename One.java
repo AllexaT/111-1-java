452cf935342969baeca66c4410e26f6a4c5b588a
AllexaT <113741846+AllexaT@users.noreply.github.com>
2022-09-23 11:42:58 +0800

    v2.0

--- One.java
+++ One.java
@@ -1,6 +1,6 @@
 public class One{
     public static void main(String[] args){
         System.out.println(args[0]);
-        System.out.println(args[1]);
+        System.out.println("v2.0");
     }
 }
