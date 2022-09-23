e95d6a68d426d3eda2b6c8fa67c78493f84f5a60
AllexaT <113741846+AllexaT@users.noreply.github.com>
2022-09-23 11:55:12 +0800

    v2.1

--- One.java
+++ One.java
@@ -1,6 +1,10 @@
 public class One{
     public static void main(String[] args){
         System.out.println(args[0]);
-        System.out.println("v2.0");
+        Nameshow("EEEEEEEEE");
+        System.out.println("v2.1");
+    }
+    public static void Nameshow(String str){
+        System.out.println("My name is " + str);
     }
 }
