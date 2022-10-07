95c1780b3aa54ad4153831ae0092cb953748d3af
AllexaT <113741846+AllexaT@users.noreply.github.com>
2022-10-07 09:02:47 +0800

    Add files via upload

--- Practice/One.java
+++ Practice/One.java
@@ -28,34 +28,6 @@ public class One{
 				break;
 			
 		}
-/*if(a>100)
-            System.out.println("你的手有問題!");
-        else if(a>89)
-            System.out.println("A");
-        else if(a>79)
-            System.out.println("B");
-        else if(a>69)
-            System.out.println("C");
-        else if(a>59)
-            System.out.println("D");
-        else if(a>-1)
-            System.out.println("E");
-        else
-            System.out.println("你要去看醫生了");
-*/
-/*      
-  下方題目可作為學科陷阱題
-  
-        if(a>=60)
-            if(a>=70)
-                System.out.println("good");
-        else
-             System.out.println("bad");
-         
-*/
         System.out.printf("\nv2.2\n");
-    }
-    public static void Nameshow(String str){
-        System.out.println("My name is " + str);
     }
 }
