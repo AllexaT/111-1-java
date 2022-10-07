76163e87f80bd04d215351312df0e652509b7de2
AllexaT <113741846+AllexaT@users.noreply.github.com>
2022-10-07 11:23:23 +0800

    Add files via upload

---
+++ Practice/221007/Quiz1ans.java
@@ -0,0 +1,12 @@
+public class Quiz1ans{
+    public static void main(String[] args){
+        int grade=0;
+        String space="";
+        while(grade<10){
+            System.out.println(grade);
+            grade++;
+            space=space+" ";
+            System.out.printf(space);
+        }
+    }
+}
