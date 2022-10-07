8422e4752663fb54c45c617e7c177914b08d2a6c
AllexaT <113741846+AllexaT@users.noreply.github.com>
2022-10-07 09:26:32 +0800

    Add files via upload

--- One.java
+++ One.java
@@ -9,16 +9,25 @@ public class One{
         Scanner input = new Scanner(System.in);
         System.out.print("輸入成績: ");
         a=input.nextInt();
-        else if(a>89 && a<101)
-            System.out.println("甲等!");
-        else if(a>79)
-            System.out.println("乙等!");
-        else if(a>69)
-            System.out.println("丙等!");
-        else if(a>59)
-            System.out.println("丁等!");
-        else if(a>-1)
-            System.out.println("好笨!");
-
+		switch (a/10){
+			case 10:
+			case 9:
+				System.out.println("A");
+				break;
+			case 8:
+				System.out.println("B");
+				break;
+			case 7:
+				System.out.println("C");
+				break;
+			case 6:
+				System.out.println("D");
+				break;
+			default:
+				System.out.println("E");
+				break;
+			
+		}
+        System.out.printf("\nv2.2\n");
     }
 }
